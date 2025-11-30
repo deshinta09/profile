5aba3c8393ded4b540c1a58f00b3d7655a00be94
deshinta <deshintaaulia065@gmail.com>
2025-11-30 13:05:05 +0700

    style: add style stiky in dashboard

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,8 +2,8 @@ export default function MainLayout({ children }) {
   const listMenu = ["Home", "Products", "Facility", "Review", "Contack"];
   return (
     <div>
-      <div className="w-full h-60">
-        <div className="flex justify-between items-center px-3 py-2">
+      <div className="w-full relative">
+        <div className="flex justify-between items-center px-3 py-2 sticky top-0">
           <div className="flex gap-3 items-center">
             <div className="rounded-full bg-amber-300 w-15 h-15 flex items-center justify-center">
               <span>img</span>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 export default function LandingPage() {
   return (
-    <div className="w-full h-160 bg-amber-600 flex items-center justify-center">
+    <div className="w-full h-145 bg-amber-600 flex items-center justify-center">
       <h1 className="text-center">Landing Page</h1>
     </div>
   );
